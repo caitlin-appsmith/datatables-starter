c7b362b30194744104cee6be722c5796efa58ec5
caitlin-appsmith <caitlin@tv83.net>
2022-04-21 20:39:36 +1000

    end of day 4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 //Here we're importing items we'll need. You can add other imports here.
 
-var table;
+let table;
 
 // exposing loadData to FileMaker Script
 window.loadData = function (json) {
+  // const data = JSON.parse(json);
+  const obj = JSON.parse(json);
+  const{data, type} = obj ;
+
+  const admissionsColumns =   [
+    {title:"School",data:"fieldData.School", visible:false},
+    {title:"Region",data:"fieldData.Region"},
+    {title:"Year",data:"fieldData.MatricYear"},
+    {title:"Decision",data:"fieldData.Decision"}];
+  const customerColumns =   [
+    {title:"City",data:"fieldData.City"},
+    {title:"Company",data:"fieldData.CompanyName"},
+    {title:"ZipCode",data:"fieldData.Zip"}];
+
+  const columns = type === "Customers" ? customerColumns : admissionsColumns;
+
+  console.log(data);
   // TODO: Load the DataTables libraries by linking to the DataTables CDN.
   //TODO: Get data from FileMaker
   //TODO: prep it for the JS.
@@ -15,11 +32,21 @@ window.loadData = function (json) {
   //https://datatables.net/examples/index
   // Create the DataTable, after destroying it if already exists
   table = $("#dtable").DataTable({
-    paging: true,
+    paging: false,
     pageLength: 20,
     searching: true,
     colReorder: true,
-    columns: [],
-    data: [],
+    columns: columns,
+    // [{title:"City",data:"fieldData.City"},{title:"Company",data:"fieldData.CompanyName"}],
+    // [{title:"City",data:"fieldData.School"},{title:"Company",data:"fieldData.Region"}],
+    // [{title:"First Name",data:"firstName", width:"20%"}, {title:"Last Name", data:"lastName"},],
+    data: data,
+    
+  //   [
+  //     {firstName:"George", lastName:"Harrison"},
+  //     {firstName:"Paul", lastName:"McCartney"},
+  //     {firstName:"John", lastName:"Lennor"},
+  //     {firstName:"Ringo", lastName:"Starr"},
+  // ],
   });
 };
